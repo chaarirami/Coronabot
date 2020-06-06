afbe6a8ec53d217fc8b723140f3b06cc948e60ee
Gondeck, Fabian <fabian.gondeck@haw-hamburg.de>
2020-06-06 14:34:07 +0200

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -9,13 +9,12 @@ import math
 
 
 class Motor:
-    motor = Adafruit_MotorHAT(addr=0x60) #Positionnierung des Roboters
-    myMotor = motor.getStepper(200,2)
-    stepsPerRevolution = 0
-    rpm =30    #60 round per Minutes
-    port = 0    #Port Number
-    distance = 0
-    steps = 0
+    motor = Adafruit_MotorHAT(addr=0x60)                                                    #Adresse des unteren Motorshield
+    myMotor = motor.getStepper(200,2)                                                       #Motor mit 200 Schritten pro Umdrehung; angeschlossen an Port 2
+    stepsPerRevolution = 0                                                                  #
+    rpm =30                                                                                 #60 round per Minutes
+    port = 0                                                                                #Port Number
+    distance = 0                                                                            #zu fahrende Distanz in m
     def __init__(self, stepsPerRevolution, port):
         self.stepsPerRevolution = stepsPerRevolution
         self.port = port
@@ -24,18 +23,16 @@ class Motor:
     def drive(self, distance):
         self.distance = distance
         self.myMotor.setSpeed(self.rpm)
-        revolutions = abs(distance/math.pi*0.1)  #Durchmesser der Räder = 100 mm
+        revolutions = abs(distance/math.pi*0.1)                                             #Verhältnis Distanz zu Umfang der Räder (mit Raddurchmesser = 100mm) 
         steps = int(2*self.stepsPerRevolution*revolutions)
-        if distance > 0:                         #Fahre vorwaerts
+        if distance > 0:                                                                    #Fahre vorwaerts
             self.myMotor.step(steps, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
-        else:                                   #Fahre rueckwaerts
+        else:                                                                               #Fahre rueckwaerts
             self.myMotor.step(steps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
-        self.steps = self.steps + steps
+        return steps
     def stop(self):
-        self.myMotor.setSpeed(1)
-    def setSpeed(self, rpm):
-        self.rpm = rpm
+        self.myMotor.setSpeed(1)                                                            #Motor stoppt
+    def setSpeed(self, rpm):    
+        self.rpm = rpm                                                                      #
     def getDistance(self):
         return self.distance
-    def getSteps:
-        return self.steps
